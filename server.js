0d1cf2e4d779ac78b0a943778fda52977c74c837
Siewdass Sf <siewdass@gmail.com>
2021-03-13 11:56:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,1 @@
-var SocketCluster = require('socketcluster');
-
-var socketCluster = new SocketCluster({
-    workers: 1,
-    brokers: 1,
-    port: 3000,
-    appName: "BB Api",
-    workerController: __dirname + '/worker.js',
-    brokerController: __dirname + '/broker.js',
-    socketChannelLimit: 1000,
-    rebootWorkerOnCrash: true
-});
-
-
+console.log( 'Hello World' )
